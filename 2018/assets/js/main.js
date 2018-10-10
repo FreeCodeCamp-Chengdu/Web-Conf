de076ef8840e2fa7425d47794795a690bc2d253b
liunnnanbing <exformation@icloud.com>
2018-10-10 11:29:36 +0800

    加入插图

--- 2018/assets/js/main.js
+++ 2018/assets/js/main.js
@@ -3,7 +3,7 @@
     var map = new AMap.Map('map', {
         center: [104.065317,30.581311],
         zoom: 14,
-        resizeEnable: true
+        resizeEnable: false
     });
 
     AMap.plugin('AMap.ToolBar',function(){
@@ -19,7 +19,7 @@
     function createInfoWindow (){
         return [
             '<div style="padding:10px;">',
-            '<p style="font-size:14px;margin-bottom:5px;font-weight:bold;">2018 成都Web前端大会会场</p>',
+            '<p style="font-size:14px;margin-bottom:5px;font-weight:bold;color:#6E6EF7">2018 成都Web前端大会会场</p>',
             '<p style="font-size:12px">地址 : 天府大道北段966号天府国际金融中心4号楼1楼1号会议厅</p>',
             '</div>'
         ].join('');
