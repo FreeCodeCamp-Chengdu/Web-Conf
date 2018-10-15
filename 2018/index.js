ecf7d712d1f86b30c8c0eed5ca12307a698476f4
TechQuery <shiy007@qq.com>
2018-10-15 19:40:51 +0800

    【优化】多处体验细节

--- 2018/index.js
+++ 2018/index.js
@@ -1,46 +1,52 @@
 (function($, WebCell, marked, Loading) {
     var request = WebCell.request,
         documentReady = WebCell.documentReady,
+        mediaReady = WebCell.mediaReady,
         ObjectView = WebCell.ObjectView;
 
     //  渲染数据
 
-    Promise.all([request('index.json'), documentReady]).then(function(data) {
-        data = data[0];
+    Promise.all([request('index.json'), documentReady])
+        .then(function(data) {
+            data = data[0];
 
-        data.company = data.company.map(function(name) {
-            return { name: name };
-        });
-
-        for (var key in data)
-            if ('detail' in data[key][0])
-                for (var i = 0; data[key][i]; i++)
-                    data[key][i].detail = marked(data[key][i].detail);
-
-        var body = new ObjectView(document.body);
-
-        body.render(data);
-
-        var intersection = new IntersectionObserver(function(entry, observer) {
-            $.each(entry, function() {
-                if (!this.isIntersecting) return;
-
-                var data = this.target.dataset;
-
-                this.target.src = data.src;
-
-                delete data.src;
-
-                observer.unobserve(this.target);
+            data.company = data.company.map(function(name) {
+                return { name: name };
             });
-        });
 
-        $('[data-src]').each(function() {
-            intersection.observe(this);
-        });
-    });
+            for (var key in data)
+                if ('detail' in data[key][0])
+                    for (var i = 0; data[key][i]; i++)
+                        data[key][i].detail = marked(data[key][i].detail);
 
-    self.addEventListener('load', Loading.closeAll.bind(Loading));
+            var body = new ObjectView(document.body);
+
+            body.render(data);
+
+            var intersection = new IntersectionObserver(function(
+                entry,
+                observer
+            ) {
+                $.each(entry, function() {
+                    if (!this.isIntersecting) return;
+
+                    var data = this.target.dataset;
+
+                    this.target.src = data.src;
+
+                    delete data.src;
+
+                    observer.unobserve(this.target);
+                });
+            });
+
+            $('[data-src]').each(function() {
+                intersection.observe(this);
+            });
+
+            return mediaReady();
+        })
+        .then(Loading.closeAll.bind(Loading));
 
     //  导航滚动
     $(document).on('click', 'a[href^="#"]', function() {
