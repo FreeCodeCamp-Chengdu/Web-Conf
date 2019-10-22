5e7b4431203b0240b5233571e8c9362ff9ccab5c
jiahao.li <jiahao.li@thoughtworks.com>
2019-10-22 21:14:00 +0800

    [refactor] replace className with postcss

--- source/page/2017/index.tsx
+++ source/page/2017/index.tsx
@@ -10,10 +10,10 @@ export function Page2017() {
     return (
         <main>
             <div className="container">
-                <header id="logo">
+                <header id={style.logo}>
                     <img src={banner} />
                 </header>
-                <div id="introduce">
+                <div id={style.introduce}>
                     <h2 className="mt-5 mb-4 text-center">
                         <span>INTRODUCE</span> <span>活动介绍</span>
                     </h2>
@@ -22,7 +22,7 @@ export function Page2017() {
                         成都社区一直以为成都技术爱好者以及用户提供一个开放的交流环境为目的，在过去的一年里，我们成功举办了30余次的社区活动，受到大家的一致好评和热烈响应。2017年已接近尾声，在组织部何青松的倡议下，我们特筹办freeCodeCamp【2017成都WEB前端交流大会】，为成都前端圈的技术工程师、爱好者提供一场前所未有的饕餮盛宴。在此，我们向您发出诚挚的邀请，此次盛会期待您的参与！
                     </p>
                 </div>
-                <div className="web-content">
+                <div className={style['web-content']}>
                     <h2 className="mt-5 mb-4 text-center">
                         <span>SPEAKER</span> <span>分享嘉宾</span>
                     </h2>
@@ -37,7 +37,7 @@ export function Page2017() {
                         ))}
                     </div>
                 </div>
-                <div className="web-content">
+                <div className={style['web-content']}>
                     <h2 className="mt-5 mb-4 text-center">
                         <span>SCHEDULE</span> <span>活动流程</span>
                     </h2>
@@ -50,7 +50,7 @@ export function Page2017() {
                         )}
                     />
                 </div>
-                <div className="web-content">
+                <div className={style['web-content']}>
                     <div>
                         <h2 className="mt-5 mb-4 text-center">
                             <span>时间地址</span>
@@ -64,8 +64,8 @@ export function Page2017() {
                         </p>
                     </div>
                 </div>
-                <div className="web-content">
-                    <div className="logos">
+                <div className={style['web-content']}>
+                    <div className={style.logos}>
                         {data.logos.map(({ title, childrens }) => (
                             <section>
                                 <h2 className="mt-5 mb-4 text-center">
@@ -84,14 +84,17 @@ export function Page2017() {
                         ))}
                     </div>
                     <div>
-                        <a href="http://cn.mikecrm.com/rdVltvT" className="btn">
+                        <a
+                            href="http://cn.mikecrm.com/rdVltvT"
+                            className={style.btn}
+                        >
                             我 要 报 名
                         </a>
                     </div>
                 </div>
             </div>
             <footer>
-                <img className="qrcode" src={qrcode} />
+                <img className={style.qrcode} src={qrcode} />
                 <p className="mt-0">
                     特别鸣谢
                     <a target="_blank" href="http://918930.lofter.com">
