bac3794b9ed230c48ba0556d79f92fd79f7e3bec
TechQuery <TechQuery@users.noreply.github.com>
2024-06-20 03:08:00 +0000

    deploy: b86c0a8d08e59c6a0946387c02cb2f483372902c

---
+++ en-US.1eee4986.js
@@ -0,0 +1,2 @@
+(0,globalThis.parcelRequire2198.register)("6cv5F",function(e,r){Object.defineProperty(e.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(e.exports,"default",{get:()=>i,set:void 0,enumerable:!0,configurable:!0});var i={review:"review",register:"register"}});
+//# sourceMappingURL=en-US.1eee4986.js.map
